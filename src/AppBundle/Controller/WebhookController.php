9b9a66d0232b3cd84bb4061440003e4ccdf012f4
webmaster <webmaster@localhost.localdomain>
2018-02-09 17:32:01 +0800

    try to use service to change pw; but initialization fail

--- src/AppBundle/Controller/WebhookController.php
+++ src/AppBundle/Controller/WebhookController.php
@@ -7,16 +7,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Process\Process;
-use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class WebhookController extends Controller
 {
 	private $git_repo_local_path;
-	public function __construct($git_repo_local_path)
-	{
-		$this->git_repo_local_path = $git_repo_local_path;
-	}
+
 	/**
 	 * @Route("/gitwebhook", name="gitwebhook")
 	 */
@@ -29,8 +24,13 @@ class WebhookController extends Controller
 				if (isset($params["repository"]["links"]["html"])){
 					$repo = $params["repository"]["name"];
 					$link = $params["repository"]["links"]["html"];
-					$ret = $this->gitFetch($repo);
-		      return new JsonResponse(array("ret" => $ret));
+                    /* @var GearmanClient $gearman */
+                    $gearman = $this->container->get('gearman');
+                    $result = $gearman
+                        ->doNormalJob('AppBundleServicesGitWorkerDummy~fetchRemoteJobDummy', json_encode(array('repo' => $repo)));
+
+                    $returnCode = $gearman->getReturnCode();
+                    return new JsonResponse(array("ret" => $returnCode));
 				}
 			}
 		}
@@ -41,24 +41,11 @@ class WebhookController extends Controller
 	 */
 	public function shellAction(Request $request)
 	{
-	    /* @var GearmanClient $gearman */
-	    $gearman = $this->container->get('gearman');
+        $gearman = $this->container->get('gearman');
         $result = $gearman
             ->doNormalJob('AppBundleServicesGitWorkerDummy~fetchRemoteJobDummy', json_encode(array('repo' => 'sucks')));
 
         $returnCode = $gearman->getReturnCode();
         return new JsonResponse(array("ret" => $returnCode));
-	}
-
-	private function gitFetch($rep)
-	{
-		$process = new Process('cd ' . $this->git_repo_local_path . ' && ./gitfetch.sh ' . $rep );
-		$process->run();
-
-		// executes after the command finishes
-		if (!$process->isSuccessful()) {
-			throw new ProcessFailedException($process);
-		}
-		return $process->getOutput();
 	}
 }
--- src/AppBundle/Services/GitWorker.php
+++ src/AppBundle/Services/GitWorker.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Services;
 
 use Mmoreram\GearmanBundle\Driver\Gearman;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 /**
  * @Gearman\Work(
@@ -14,6 +16,12 @@ use Mmoreram\GearmanBundle\Driver\Gearman;
  */
 class GitWorker
 {
+    private $git_repo_local_path;
+
+    public function __construct($git_repo_local_path)
+    {
+        $this->git_repo_local_path = $git_repo_local_path;
+    }
 
     /**
      * Test method to run as a job
@@ -30,8 +38,21 @@ class GitWorker
     public function fetchRemote(\GearmanJob $job)
     {
         $data = json_decode($job->workload(),true);
-        echo $data['repo'] . PHP_EOL;
+        $this->git_repo_local_path = '/home/webmaster/git';
+        echo $data['repo'] . "\n" . $this->git_repo_local_path . "\n" . PHP_EOL;
 
         return true;
+    }
+
+    private function gitFetch($rep)
+    {
+        $process = new Process('cd ' . $this->git_repo_local_path . ' && ./gitfetch.sh ' . $rep );
+        $process->run();
+
+        // executes after the command finishes
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+        return $process->getOutput();
     }
 }
