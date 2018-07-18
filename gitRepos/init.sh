55277b4476df4abfa474ca378f595a16ae639f07
wingzero <wingzzz2003@hotmail.com>
2018-07-18 21:09:40 +0800

    directly clone repo by hard code backup script

---
+++ gitRepos/init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+git clone https://bitbucket.org/codingguys/fbcrawler.git --mirror
+git clone https://bitbucket.org/codingguys/mnemonoapi.git --mirror
+git clone https://bitbucket.org/codingguys/weibocrawler.git --mirror
+git clone https://bitbucket.org/codingguys/macaunewscrawler.git --mirror
+git clone https://bitbucket.org/codingguys/mnemonoweb.git --mirror
+git clone https://bitbucket.org/codingguys/mon-tools.git --mirror
+git clone https://bitbucket.org/codingguys/mhkodooreports.git --mirror
+git clone https://bitbucket.org/codingguys/mhkvmbackupscript.git --mirror
+git clone https://bitbucket.org/codingguys/instagramcrawler.git --mirror
+git clone https://bitbucket.org/codingguys/melonyeahapi.git --mirror
+
+git clone https://bitbucket.org/codingguys/linodeinitscript.git --mirror
+git clone https://bitbucket.org/codingguys/mnemonoios.git --mirror
+git clone https://bitbucket.org/codingguys/melonyeahios.git --mirror
+git clone https://bitbucket.org/codingguys/melonyeahweb.git --mirror
+git clone https://bitbucket.org/codingguys/iagbb.git --mirror
+git clone https://bitbucket.org/codingguys/mhkodooimport.git --mirror
+git clone https://bitbucket.org/codingguys/fbpagelike.git --mirror
+git clone https://bitbucket.org/codingguys/game-post.git --mirror
+git clone https://bitbucket.org/codingguys/android-nfc-tag-read-write.git --mirror
+git clone https://bitbucket.org/codingguys/mn-map.git --mirror
+
+git clone https://bitbucket.org/codingguys/mnemono.com.git --mirror
+git clone https://bitbucket.org/codingguys/ftp.git --mirror
+git clone https://bitbucket.org/codingguys/propertyandroid.git --mirror
+git clone https://bitbucket.org/codingguys/property.git --mirror
+git clone https://bitbucket.org/codingguys/melonyeahwebapp.git --mirror
+git clone https://bitbucket.org/codingguys/my-app.git --mirror
+git clone https://bitbucket.org/codingguys/nfc-misc.git --mirror
+git clone https://bitbucket.org/codingguys/codebehavior.git --mirror
+git clone https://bitbucket.org/codingguys/doc-sample.git --mirror
+git clone https://bitbucket.org/codingguys/android-hce-demo.git --mirror
+
+git clone https://bitbucket.org/codingguys/busdatacollectionandroid.git --mirror
+git clone https://bitbucket.org/codingguys/busdatacollectionphp.git --mirror
